6daf7d34fe8c0a13e22d1011a9560a7957875c7d
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-03-20 16:31:28 +0700

    feat : created Startup Create page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
+import { Toaster } from "sonner";
 
 const Layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <main className="font-work-sans">
       <Navbar></Navbar>
       {children}
+      <Toaster></Toaster>
     </main>
   );
 };
---
+++ app/(root)/startup/create/page.tsx
@@ -0,0 +1,19 @@
+import { auth } from "@/app/auth";
+import StartupForm from "@/components/StartupForm";
+import { redirect } from "next/dist/server/api-utils";
+import React from "react";
+
+const page = async () => {
+  const session = await auth();
+  if (!session) redirect("/");
+  return (
+    <>
+      <section className="pink_container !min-h-[230px]">
+        <h1 className="heading">Submit Startup</h1>
+      </section>
+      <StartupForm></StartupForm>
+    </>
+  );
+};
+
+export default page;
